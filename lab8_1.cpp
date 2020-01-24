07e0bd5d811c7e1ab95ccdb069332fd23c30527a
Erzengel792 <tanadol_boat@hotmail.com>
2020-01-24 15:10:02 +0700

    Edited V.1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main(){	
 
+	       double prev,inter,total,pay,last,cal;
+		   int num = 1;
+cout<< "Enter initial loan:" ;
+cin>>prev;
+cout<<"Enter interest rate per year (%):" ;
+cin>>inter;
+cout<<"Enter amount you can pay per year:";
+cin>>pay;
+
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -15,16 +24,27 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	
+	
+	
+for(;last !=0; num++){
+		
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << num; 
+	cout << setw(13) << left << prev;
+	cal=prev*(inter/100);
+	cout << setw(13) << left << cal;
+	total = cal+prev;
+	cout << setw(13) << left << total;
+	if(total < pay) pay = total;
+	cout << setw(13) << left << pay;
+	last = total - pay;
+	cout << setw(13) << left << last;
+	prev = last;
+	cout << "\n";
+
+}	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N , int M){
+	int countM = M;
+if(N <= 0) cout<<"Invalid input";
+else if (M <=0) cout<< "Invalid input";
+else {
+	for(int countN = N; countN > 0 ;countN--){
+		for(int countM = M; countM > 0 ;countM--){
+			cout<<"O";
+		}
+	
+	cout<< "\n";
+	}
+
+	}
+}
 
 int main(){
 	
